9993e909e482f914fd7d1ce5a943db1de5928bf0
Diman_Stamatov <d.stamatov@yahoo.com>
2023-06-08 16:13:40 +0300

    Synking

---
+++ WEB Group Project-Forum System/WEB Group Project-Forum System/Repositories/Interfaces/IEmailsRepository.cs
@@ -0,0 +1,13 @@
+﻿using WEB_Group_Project_Forum_System.Models;
+
+namespace WEB_Group_Project_Forum_System.Repositories.Interfaces
+{
+    public interface IEmailsRepository
+    {
+        List<Tag> GetAll();
+        Tag GetById(int id);
+        Tag GetByName(string name);
+        Tag Create(string name);
+        Tag Delete(string name);
+    }
+}
